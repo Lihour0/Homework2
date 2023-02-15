65b4de7bc7bae2d3debfe6ee075a6b7ba75c2adb
Konmus <konmus0@gmail.com>
2023-02-15 12:26:38 +0700

    Added Validation to nonNegative Number

--- homework2.3/homework4.cpp
+++ homework2.3/homework4.cpp
@@ -7,6 +7,17 @@ void InputOnlyNum()
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
+
+template <typename T> bool NotNegative(T input)
+{
+    if (!(input >= 0))
+    {
+        std::cout << "This input can't be negative\n";
+        return true;
+    }
+    return false;
+}
+
 template <typename T> T averageDaily(T netBalance, T d1, T payment, T d2)
 {
     return (netBalance * d1 - payment * d2) / d1;
@@ -28,31 +39,31 @@ int main()
     double interestMonthly = 0;
 
     std::cout << "Net Balance: ";
-    while (!(std::cin >> netBalance))
+    while (!(std::cin >> netBalance) || NotNegative(netBalance))
     {
         InputOnlyNum();
         std::cout << "Net Balance: ";
     }
     std::cout << "Payment Made: ";
-    while (!(std::cin >> payment))
+    while (!(std::cin >> payment) || NotNegative(payment))
     {
         InputOnlyNum();
         std::cout << "Payment Made: ";
     }
     std::cout << "Number of day in the billing cycle: ";
-    while (!(std::cin >> d1))
+    while (!(std::cin >> d1) || NotNegative(d1))
     {
         InputOnlyNum();
         std::cout << "Number of day in the billing cycle: ";
     }
     std::cout << "Number of days payment is made before billing cycle: ";
-    while (!(std::cin >> d2))
+    while (!(std::cin >> d2) || NotNegative(d2))
     {
         InputOnlyNum();
         std::cout << "Number of days payment is made before billing cycle: ";
     }
     std::cout << "Monthly Interest: ";
-    while (!(std::cin >> monthlyInterestRate))
+    while (!(std::cin >> monthlyInterestRate) || NotNegative(monthlyInterestRate))
     {
         InputOnlyNum();
         std::cout << "Monthly Interest: ";
